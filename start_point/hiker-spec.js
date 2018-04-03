5027fd967532200ddd2fbfb5085579369cf996ef
Jon Jagger <jon@jaggersoft.com>
2018-04-03 08:56:46 +0100

    [ci skip] minor refactoring

--- start_point/hiker-spec.js
+++ start_point/hiker-spec.js
@@ -2,8 +2,8 @@
 
 const answer = require('./hiker.js');
 
-describe("answer", function() {
-  it("to life the universe and everything", function() {
+describe('answer', function() {
+  it('to life the universe and everything', function() {
     expect(answer()).toEqual(42);
   });
 });
