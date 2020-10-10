5962bd7604a869005fc13e0fe85325f6e587ba07
Jon Jagger <jon@jaggersoft.com>
2020-10-10 10:39:26 +0100

    Remove dead js-hint pattern from red_amber_green.rb

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,11 +1,8 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  js_hint_pattern = /^(\d+) error(s?)/
   pattern = /^(\d+) spec(s?), (\d+) failure(s?)/
-  if js_hint_pattern.match(output)
-    :amber
-  elsif match = pattern.match(output)
+  if match = pattern.match(output)
     match[3] == '0' ? :green : :red
   else
     :amber
